5229f066757311d9f57b1b7757c69ff97a3a89dd
Jonathan Rinciari <jrinciaritosd@gmail.com>
2018-10-29 23:23:04 -0400

    added basics of textOnDevice recognition

--- src/screens/homescreen/homescreen.ts
+++ src/screens/homescreen/homescreen.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+import { MLKitRecognizeTextResult } from "nativescript-plugin-firebase/mlkit/textrecognition";
+const firebase = require("nativescript-plugin-firebase");
 
 @Component({
   selector: 'homescreen',
@@ -11,6 +12,10 @@ export class HomeScreen {
   constructor(){}
 
   onTap() {
-    console.log('test');
+    firebase.mlkit.textrecognition.recognizeTextOnDevice({
+      image: './test.jpeg' // a NativeScript Image or ImageSource, see the demo for examples
+    }).then((result: MLKitRecognizeTextResult) => { // just look at this type to see what else is returned
+      console.log(result.text ? result.text : "");
+    }).catch(errorMessage => console.log("ML Kit error: " + errorMessage));
   }
 }
